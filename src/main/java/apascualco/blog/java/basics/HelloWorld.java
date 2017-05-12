2a7719ddae809e5f1d0e36f27554bc6431dabda8
alberto <apascualco@gmail.com>
2017-05-12 19:56:54 +0200

    Initial commit

---
+++ src/main/java/apascualco/blog/java/basics/HelloWorld.java
@@ -0,0 +1,24 @@
+package apascualco.blog.java.basics;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Clase HelloWorld");
+    }
+}
+
+class ClaseNoPublica {
+
+    public static void main(String[] args) {
+        System.out.println("Clase no publica");
+    }
+
+}
+
+class ClaseNoPublicaDos {
+
+    public static void main(String[] args) {
+        System.out.println("Clase no publica dos");
+    }
+
+}
